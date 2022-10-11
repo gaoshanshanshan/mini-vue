347ccaac11aba32651f49d42c89b8bbe8c433017
gaoshan <1085077154@qq.com>
2022-10-11 09:56:39 +0800

    feat: 实现isReactive和isReadonly

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,16 +1,18 @@
-import { reactive, readonly } from '../reactive';
+import { reactive, readonly, isReadonly } from "../reactive";
 describe("readonly", () => {
   it("happy path", () => {
     const original = { foo: 1 };
     const obj = readonly(original);
     expect(obj).not.toBe(original);
     expect(obj.foo).toBe(1);
+    expect(isReadonly(obj)).toBe(true);
+    expect(isReadonly(original)).toBe(false);
   });
 
   it("wran when call set", () => {
     console.warn = jest.fn();
     const obj = readonly({ foo: 1 });
-    obj.foo++
-    expect(console.warn).toBeCalled()
+    obj.foo++;
+    expect(console.warn).toBeCalled();
   });
 });
