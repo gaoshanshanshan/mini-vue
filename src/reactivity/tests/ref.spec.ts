936670c0a65de105b2e950980678b411cfbb7417
gaoshan <1085077154@qq.com>
2022-10-13 15:48:09 +0800

    feat: proxyRef实现

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,5 @@
 import { effect } from "../effect";
-import { isRef, ref, unRef } from "../ref";
+import { isRef, proxyRef, ref, unRef } from "../ref";
 
 describe("ref", () => {
   it("happy path", () => {
@@ -40,13 +40,32 @@ describe("ref", () => {
 
   it("isRef", () => {
     const a = ref(1);
-    expect(isRef(a)).toBe(true)
-    expect(isRef(1)).toBe(false)
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
   });
 
   it("unRef", () => {
     const a = ref(1);
-    expect(unRef(a)).toBe(1)
-    expect(unRef(1)).toBe(1)
+    expect(unRef(a)).toBe(1);
+    expect(unRef(1)).toBe(1);
+  });
+
+  it("proxyRef", () => {
+    const user = {
+      age: ref(10),
+      name: "xiaoming",
+    };
+    const proxyUser = proxyRef(user);
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    expect(proxyUser.name).toBe("xiaoming");
+
+    proxyUser.age = 20;
+    expect(user.age.value).toBe(20);
+    expect(proxyUser.age).toBe(20);
+
+    proxyUser.age = ref(10);
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
   });
 });
